fdbc623800a3194e4d751c487d6270d0f3fcc1b0
prsmart2150@gmail.com <praveenprajapati0810@gmail.com>
2024-09-13 02:02:58 +0530

    add the authentication code and other api request

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const authRoutes = require('./auth');
 const authMiddleware = require('./authMiddleware');
@@ -7,32 +8,23 @@ dotenv.config();
 
 const app = express();
 
+// app.use(cors());
 app.use(express.json());
-app.use('/auth', authRoutes);
+app.use(
+  cors({
+    origin: 'http://localhost:3000', // Allow requests only from this origin
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allow specific methods
+    credentials: true, // If using cookies for authentication
+  })
+);
 
 const PORT = process.env.DB_PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-// Example of a protected route
-app.get('/profile', authMiddleware, async (req, res) => {
-  try {
-    const user = await db('users').where({ id: req.userId }).first();
-    if (!user) {
-      return res.status(404).json({ error: 'User not found.' });
-    }
+app.use('/auth', authRoutes);
 
-    res.json({
-      first_name: user.first_name,
-      last_name: user.last_name,
-      email: user.email,
-      role: user.role,
-    });
-  } catch (error) {
-    res.status(500).json({ error: 'Error fetching user data.' });
-  }
-});
 
 app.use('/', (req, res) => {
   res.send(`<div>
--- auth.js
+++ auth.js
@@ -1,16 +1,14 @@
-// auth.js
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const db = require('./db'); // Import Knex instance
+const db = require('./db');
+const authMiddleware = require('./authMiddleware');
 const router = express.Router();
 
-// Registration Route
 router.post('/register', async (req, res) => {
   const { first_name, last_name, email, password, role } = req.body;
 
   try {
-    // Check if user already exists
     const existingUser = await db('users').where({ email }).first();
     if (existingUser) {
       return res
@@ -18,10 +16,8 @@ router.post('/register', async (req, res) => {
         .json({ error: 'User with this email already exists.' });
     }
 
-    // Hash the password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Insert new user into PostgreSQL
     await db('users').insert({
       first_name,
       last_name,
@@ -36,31 +32,35 @@ router.post('/register', async (req, res) => {
   }
 });
 
-// Login Route
 router.post('/login', async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Find user by email
     const user = await db('users').where({ email }).first();
     if (!user) {
       return res.status(400).json({ error: 'Invalid credentials.' });
     }
 
-    // Compare password
     const validPassword = await bcrypt.compare(password, user.password);
     if (!validPassword) {
       return res.status(400).json({ error: 'Invalid credentials.' });
     }
 
-    // Generate JWT token
     const token = jwt.sign(
-      { id: user.id, role: user.role }, // Include the user's role in the token
+      { id: user.id, role: user.role },
       process.env.JWT_SECRET,
       { expiresIn: '1h' }
     );
 
-    res.json({ token });
+    const userWithoutPassword = {
+      id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      role: user.role,
+    };
+
+    res.json({ token, user: userWithoutPassword });
   } catch (error) {
     res.status(500).json({ error: 'Error logging in.' });
   }
@@ -73,11 +73,38 @@ router.get('/users', async (req, res) => {
       'first_name',
       'last_name',
       'email',
-      'role'
+      'role',
+      'password'
     );
     res.json(users);
   } catch (error) {
     res.status(500).json({ error: 'Error fetching users.' });
+  }
+});
+
+router.get('/profile/:id', authMiddleware, async (req, res) => {
+  const { id } = req.params;
+
+  console.log('🚀 ~ file: auth.js:89 ~ router.get ~ id:', id);
+
+  try {
+    const user = await db('users').where({ id }).first();
+
+    console.log('🚀 ~ file: auth.js:95 ~ router.get ~ user:', user);
+
+    if (!user) {
+      return res.status(404).json({ error: 'User not found.' });
+    }
+
+    res.json({
+      id: user.id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      email: user.email,
+      role: user.role,
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching user data.' });
   }
 });
 
